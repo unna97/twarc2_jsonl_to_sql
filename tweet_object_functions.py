e1801f58fb860a5929cf3409019192a54a5c1e19
unna97 <31486108+unna97@users.noreply.github.com>
2022-07-14 02:36:04 +0530

    processing for referenced tweet table

--- tweet_object_functions.py
+++ tweet_object_functions.py
@@ -15,6 +15,8 @@ class TweetObject_to_DataTable:
             "id",
             "lang",
         ]
+        self.columns_from_original_processed = self.columns_in_tweet_table
+        self.tables_created = {}
 
     def public_metric_column_processing(self):
         new_cols = self.base_data["public_metrics"].iloc[0].keys()
@@ -24,3 +26,41 @@ class TweetObject_to_DataTable:
             )
         self.base_data.drop(columns=["public_metrics"], inplace=True)
         self.columns_in_tweet_table.extend(new_cols)
+        self.columns_from_original_processed.append("public_metrics")
+
+    def referenced_tweets_processing(self):
+        """
+        This functions creates retweet_tweet_mapping, quote_tweet_mapping, reply_tweet_mapping
+        """
+        columns_needed = ["id", "in_reply_to_user_id", "referenced_tweets"]
+
+        data = self.base_data[columns_needed].explode("referenced_tweets").dropna()
+        data.rename(columns={"id": "tweet_id"}, inplace=True)
+
+        data["referenced_tweet_id"] = data["referenced_tweets"].apply(lambda x: x["id"])
+        data["tweet_type"] = data["referenced_tweets"].apply(lambda x: x["type"])
+
+        columns_for_each = {
+            "quoted": ["tweet_id", "tweet_type", "referenced_tweet_id"],
+            "retweeted": ["tweet_id", "tweet_type", "referenced_tweet_id"],
+            "replied_to": [
+                "tweet_id",
+                "tweet_type",
+                "referenced_tweet_id",
+                "in_reply_to_user_id",
+            ],
+        }
+
+        for key in columns_for_each:
+            self.tables_created[key + "_tweet_mapping"] = data[
+                data["tweet_type"] == key
+            ][columns_for_each[key]]
+
+        self.columns_from_original_processed.extend(columns_needed)
+        self.columns_from_original_processed = list(
+            set(self.columns_from_original_processed)
+        )
+
+    def processing_overall(self):
+        self.public_metric_column_processing()
+        self.referenced_tweets_processing()
