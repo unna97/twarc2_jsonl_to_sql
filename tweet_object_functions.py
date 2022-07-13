fc2e93876258be4e00659a04b7e237766190a802
unna97 <31486108+unna97@users.noreply.github.com>
2022-07-14 01:59:15 +0530

    black formattig tweet_object_functions.py file

--- tweet_object_functions.py
+++ tweet_object_functions.py
@@ -2,19 +2,25 @@ import pandas as pd
 
 
 class TweetObject_to_DataTable:
-
-    def __init__(self, tweets_df:pd.DataFrame):
+    def __init__(self, tweets_df: pd.DataFrame):
         self.base_data = tweets_df
-        self.columns_in_tweet_table = ['author_id', 'possibly_sensitive', 'conversation_id', 'source', 
-                                'reply_settings', 'text', 'created_at', 'id', 'lang']
-    
+        self.columns_in_tweet_table = [
+            "author_id",
+            "possibly_sensitive",
+            "conversation_id",
+            "source",
+            "reply_settings",
+            "text",
+            "created_at",
+            "id",
+            "lang",
+        ]
 
     def public_metric_column_processing(self):
-        new_cols = self.base_data['public_metrics'].iloc[0].keys()
+        new_cols = self.base_data["public_metrics"].iloc[0].keys()
         for key in new_cols:
-            self.base_data[key] = self.base_data['public_metrics'].apply(lambda x: x[key])
-        self.base_data.drop(columns=['public_metrics'], inplace=True)
+            self.base_data[key] = self.base_data["public_metrics"].apply(
+                lambda x: x[key]
+            )
+        self.base_data.drop(columns=["public_metrics"], inplace=True)
         self.columns_in_tweet_table.extend(new_cols)
-
-
-
